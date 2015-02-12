45339436108980a3271b3e62553d27f772a99cbb
ComFreek <comfreek@outlook.com>
2015-02-12 14:18:38 +0100

    Add braces to for

--- core/Connection.js
+++ core/Connection.js
@@ -10,6 +10,7 @@ var Connection = module.exports = function(size){
   this.closed = false;
   this.usedslots = 0;
   this.upstreamProcsUnclosed = 0; 
-  for (var i = 0; i < size; i++)
+  for (var i = 0; i < size; i++) {
     this.array[i] = null;
+  }
 };
