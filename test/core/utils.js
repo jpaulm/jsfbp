3b2648c5a57c9dcb86f39a8d2f08dfe243840098
ComFreek <comfreek@outlook.com>
2015-02-13 16:10:18 +0100

    Added test for Enum

---
+++ test/core/utils.js
@@ -0,0 +1,25 @@
+var Enum = require('../../core/utils.js').Enum;
+
+describe('Enum', function() {
+  it('should create an enum and provide a lookup function', function() {
+    var myEnum = Enum(['a', 'b', 'c']);
+    expect(myEnum).to.have.ownProperty('a')
+              .and.to.have.ownProperty('b')
+              .and.to.have.ownProperty('c');
+    expect(myEnum.__lookup(myEnum.a)).to.equal('a');
+    expect(myEnum.__lookup(myEnum.b)).to.equal('b');
+    expect(myEnum.__lookup(myEnum.c)).to.equal('c');
+  });
+  
+  it('should not be writable', function() {
+    var myEnum = Enum(['a']);
+    
+    var prevValue = myEnum.a;
+    myEnum.a = -1;
+    expect(myEnum.a).to.equal(prevValue);
+    
+    var prevLookupFunction = myEnum.__lookup;
+    myEnum.__lookup = 100;
+    expect(myEnum.__lookup).to.equal(prevLookupFunction);
+  });
+});
