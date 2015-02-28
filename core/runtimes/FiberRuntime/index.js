31c69e5f0cf82282ffd390f0c46e71caa8a7a3f0
ComFreek <comfreek@outlook.com>
2015-02-28 13:46:19 +0100

    Cleaned up FiberRuntime run method

--- core/runtimes/FiberRuntime/index.js
+++ core/runtimes/FiberRuntime/index.js
@@ -80,148 +80,135 @@ FiberRuntime.prototype.setCallbackPending = function(b) {
 
 FiberRuntime.prototype.run = function (processes, options, callback) {
   this._list = processes;
+  this._count = this._list.length;
   
   this._tracing = global.tracing = Boolean(options.trace);
   
   var self = this;
+
   Fiber(function() {
-    self._actualRun.call(self, options.trace);
+    var startTime = Date.now();
+    console.log('Start time: ' + startTime);
+    
+    self._actualRun.call(self);
+
+    console.log('Elapsed time in secs: ' + (Date.now() - startTime));
+    
     callback(null);
   }).run();
 };
 
-// Fibre running scheduler
-FiberRuntime.prototype._actualRun = function (trace) {
-  var d = new Date();
-  var st = d.getTime();
-  console.log('Start time: ' + d.toISOString());
+FiberRuntime.prototype._createFiber = function (process) {
+  if (this._tracing) {
+    console.log('creating new fiber for ' + process.name);
+  }
+  process.fiber = new Fiber(process.func.bind(null, this));
+  process.fiber.fbpProc = process;
+  process.status = Process.Status.ACTIVE;
+  
+  return process;
+};
 
-  this._tracing = Boolean(trace);
-
-  this._count = this._list.length;
-
+FiberRuntime.prototype._hasDeadLock = function () {
   for (var i = 0; i < this._list.length; i++) {
-    var selfstarting = true;
-    for (var j = 0; j < this._list[i].inports.length; j++) {
-      var k = this._list[i].inports[j];
-      if (!k[1].conn instanceof IIPConnection) {
-        selfstarting = false;
-      }
-    }
-
-    if (selfstarting) {
-      this._queue.push(this._list[i]);
-    }
-  }
-
-  while (true) {
-    var x = this._queue.shift();
-    while (x != undefined) {      
-      if (x.fiber == null) {
-        if (this._tracing)
-           console.log('creating new fiber for ' + x.name);
-        x.fiber = new Fiber(x.func.bind(null, this));
-        x.fiber.fbpProc = x;
-        x.status = Process.Status.ACTIVE;
-      }
-
-      if (x.status != Process.Status.CLOSED) {
-        if (x.status == Process.Status.DORMANT && upconnsclosed(x)) {
-          this._close(x);
-        }
-        else {
-          if (this._tracing) {
-            if (x.yielded) {
-              console.log(x.name + ' fiber resumed');
-            }
-            else {
-              if (x.cbpending) {
-                console.log(x.name + ' fiber callback run');
-              }
-              else {
-                console.log(x.name + ' fiber started');
-              }
-            }
-          }
-
-          //------------------    
-          x.fiber.run(x.result);
-          //------------------
-
-          x.data = null;
-          if (this._tracing) {
-            console.log(x.name + ' yielded: ' + x.yielded + ', cbpending: ' + x.cbpending);
-            if (x.yielded) {
-              console.log(x.name + ' fiber yielded');
-            }
-            else {
-              if (x.cbpending) {
-                console.log(x.name + ' fiber awaiting callback');
-              }
-              else {
-                console.log(x.name + ' fiber ended');
-              }
-            }
-          }
-          if (!x.yielded && !x.cbpending) {
-            if (!upconnsclosed(x)) {
-              x.status = Process.Status.DORMANT;
-              this._queue.push(x);
-              for (var j = 0; j < x.inports.length; j++) {
-                var k = x.inports[j];
-                if (k[1].conn instanceof IIPConnection) {
-                  k[1].conn.closed = false;
-                }
-              }
-            }
-            else {
-              this._close(x);
-            }
-          }
-        }
-      }
-      x = this._queue.shift();
-    }
-
-    if (this._count <= 0) {
-      break;
-    }
-    var deadlock = true;
-    for (var i = 0; i < this._list.length; i++) {
-      if (this._list[i].cbpending || this._list[i].status == Process.Status.ACTIVE) {
-        deadlock = false;
-        break;
-      }
-    }
-    if (deadlock) {
-      console.log('Deadlock detected');
-      for (var i = 0; i < this._list.length; i++) {
-        console.log('- Process status: ' + this._list[i].getStatusString() + ' - ' + this._list[i].name);
-      }
-      throw '';
-    }
-    sleep(100);
-  }
-
-  d = new Date();
-  var et = d.getTime();
-  et -= st;
-  et /= 1000;
-  console.log('Elapsed time in secs: ' + et.toFixed(3));
-}
-
-function upconnsclosed(proc) {
-  for (var j = 0; j < proc.inports.length; j++) {
-    var k = proc.inports[j];
-    if (k[1].conn instanceof IIPConnection) {
-      continue;
-    }
-    if (!(k[1].conn.closed) || k[1].conn.usedslots > 0) {
+    if (this._list[i].cbpending || this._list[i].status == Process.Status.ACTIVE) {
       return false;
     }
   }
   return true;
-}
+};
+
+FiberRuntime.prototype._genInitialQueue = function () {
+  var self = this;
+  this._queue = [];
+  
+  this._list.forEach(function (process) {
+    var shallAdd = process.inports.every(function (inport) {
+      return !(inport[1].conn instanceof IIPConnection);
+    });
+    
+    if (shallAdd || true) {
+      self._queue.push(process);
+    }
+  });
+  return this._queue;
+};
+
+FiberRuntime.prototype._logProcessInfo = function (proc) {
+  if (this._tracing) {
+    console.log({name: proc.name, yielded: proc.yielded, cbpending: proc.cbpending});
+  }
+};
+
+FiberRuntime.prototype._areUpConnsClosed = function (proc) {
+  return proc.inports.every(function (inport) {
+    if (inport[1].conn instanceof IIPConnection) {
+      return true;
+    }
+    else if (!(inport[1].conn.closed) || inport[1].conn.usedslots > 0) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  });
+};
+
+// Fibre running scheduler
+FiberRuntime.prototype._actualRun = function (trace) {
+  this._queue = this._genInitialQueue();
+  
+  while (true) {
+    this._tick();
+
+    if (this._count <= 0) {
+      break;
+    }
+    
+    if (this._hasDeadLock()) {
+      console.log('Deadlock detected');
+      for (var i = 0; i < this._list.length; i++) {
+        console.log('- Process status: ' + this._list[i].getStatusString() + ' - ' + this._list[i].name);
+      }
+      throw 'DEADLOCK';
+    }
+    sleep(100);
+  }
+};
+
+FiberRuntime.prototype._tick = function () {
+  var x = this._queue.shift();
+  while (x != undefined) {      
+    if (x.fiber == null) {
+      x = this._createFiber(x);
+    }
+    
+    if (x.status == Process.Status.DORMANT && this._areUpConnsClosed(x)) {
+      this._close(x);
+    }
+    else if (x.status != Process.Status.CLOSED) {        
+      x.fiber.run(x.result);
+      x.data = null;
+      
+      if (!x.yielded && !x.cbpending) {
+        if (this._areUpConnsClosed(x)) {
+          this._close(x);
+        }
+        else {
+          x.status = Process.Status.DORMANT;
+          this._queue.push(x);
+          for (var i = 0; i < x.inports.length; i++) {
+            var inport = x.inports[i];
+            if (inport[1].conn instanceof IIPConnection) {
+              inport[1].conn.closed = false;
+            }
+          }
+        }
+      }
+    }
+    x = this._queue.shift();
+  }
+};
 
 function sleep(ms) {
   var fiber = Fiber.current;
