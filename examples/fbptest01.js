2527dd13f1d76dda54aa3e392b86e66aa6187e76
jpaulm <jpaulmorr@gmail.com>
2015-04-06 17:14:51 -0400

    Reducing count of IPs - it was left at volume test level by mistake

--- examples/fbptest01.js
+++ examples/fbptest01.js
@@ -8,7 +8,7 @@ var copier = network.defProc('./components/copier.js');
 var recvr  = network.defProc('./components/recvr.js');
 // var recvr = fbp.defProc(require('../components/recvr.js'), 'recvr'); // equivalent
 
-network.initialize(sender, 'COUNT', '100000000');
+network.initialize(sender, 'COUNT', '2000');
 network.connect(sender, 'OUT', copier, 'IN', 5);
 network.connect(copier, 'OUT', recvr, 'IN', 5);
 
