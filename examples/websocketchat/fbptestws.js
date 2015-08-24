05daa5bffacb591612b4e6ae7d8f0cbdfda9170d
Chris Brody <brodybits@litehelpers.net>
2015-08-24 16:00:24 +0200

    Rename examples/websocketchat/wschatresp.js to examples/websocketchat/wschatsend.js

--- examples/websocketchat/fbptestws.js
+++ examples/websocketchat/fbptestws.js
@@ -3,13 +3,13 @@ var fbp = require('../..');
 // --- define network ---
 var network = new fbp.Network();
 
-var respond  = network.defProc(require('./wschatresp'));
-var simproc  = network.defProc(require('./wssimproc'));
 var receiver = network.defProc(require('./wschatrecv'));
+var simproc  = network.defProc(require('./wssimproc'));
+var send = network.defProc(require('./wschatsend'));
 
 network.initialize(receiver, 'PORTNO', '9003');
 network.connect(receiver, 'OUT', simproc, 'IN', 6);
-network.connect(simproc, 'OUT', respond, 'IN', 6);
+network.connect(simproc, 'OUT', send, 'IN', 6);
 
 // --- run ---
 var fiberRuntime = new fbp.FiberRuntime();
