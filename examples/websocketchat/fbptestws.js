c47beb3014f36c10727a31291b38d77f51c03633
Chris Brody <brodybits@litehelpers.net>
2015-08-24 15:14:56 +0200

    Custom copies of ws recv/resp components for websocketchat example

--- examples/websocketchat/fbptestws.js
+++ examples/websocketchat/fbptestws.js
@@ -3,9 +3,9 @@ var fbp = require('../..');
 // --- define network ---
 var network = new fbp.Network();
 
-var respond  = network.defProc(require('../../components/wsresp'));
+var respond  = network.defProc(require('./wschatresp'));
 var simproc  = network.defProc(require('./wssimproc'));
-var receiver = network.defProc(require('../../components/wsrecv'));
+var receiver = network.defProc(require('./wschatrecv'));
 
 network.initialize(receiver, 'PORTNO', '9003');
 network.connect(receiver, 'OUT', simproc, 'IN', 6);
---
+++ examples/websocketchat/wschatrecv.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var IP = require('../../core/IP')
+  , WebSocketServer = require('ws').Server;
+
+module.exports = function wschatrecv(runtime) {
+  var inport = this.openInputPort('PORTNO');
+  var ip = inport.receive();
+  var portno = ip.contents;
+  var wss = new WebSocketServer({ port: portno });
+  var ws = null;
+  while (true) {
+    var result = runtime.runAsyncCallback(genWsReceiveFun(runtime, wss, ws, this));
+    console.log('wsrecv callback complete: ' + this.name);
+    //console.log(result);
+    if (result[1].endsWith('@kill')) {          
+      break;
+    }
+
+    console.log(result);
+    var outport = this.openOutputPort('OUT');
+    outport.send(this.createIPBracket(IP.OPEN));
+    outport.send(this.createIP(result[0]));
+    outport.send(this.createIP(result[1]));
+    outport.send(this.createIPBracket(IP.CLOSE));
+  }
+  
+  wss.close();
+  this.dropIP(ip);
+}
+
+function genWsReceiveFun(runtime, wss, ws, proc) {
+  return function (done) {
+    wss.on('connection', function connection(ws) {
+      ws.on('message', function incoming(message) {
+        console.log('running callback for: ' + proc.name);
+        done([ws, message]);
+      });
+      ws.send('connected!');
+    });
+    console.log('wsrecv pending: ' + proc.name);
+  };
+}
+
+String.prototype.endsWith = function (s) {
+  return this.length >= s.length && this.substr(this.length - s.length) == s;
+}
