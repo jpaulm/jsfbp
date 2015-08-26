e2959da3c3ca5a51f4ee11c9c8a3443709f7bfcc
Chris Brody <brodybits@litehelpers.net>
2015-08-26 19:09:49 +0200

    Add sample HTTP server, with custom components based on the websocketchat components, following the example in: http://blog.modulus.io/build-your-first-http-server-in-nodejs

---
+++ examples/httpserver/httprecv.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var IP = require('../../core/IP')
+  //, WebSocketServer = require('ws').Server
+  , http = require('http');
+
+module.exports = function httprecv(runtime) {
+  var inport = this.openInputPort('PORTNO');
+  var outport = this.openOutputPort('OUT');
+
+  var ip = inport.receive();
+  var portno = ip.contents;
+  var server = http.createServer(handleWebRequest);
+
+  runtime.runAsyncCallback(genListenFun(runtime, server, portno, this));
+
+  while (true) {
+    var result = runtime.runAsyncCallback(genWsReceiveFun(runtime, server, portno, this));
+    //console.log('wsrecv callback complete: ' + this.name);
+
+    for (var i=0; i<result.length; ++i) {
+      var r = result[i];
+      outport.send(this.createIPBracket(IP.OPEN));
+      outport.send(this.createIP(r.req));
+      outport.send(this.createIP(r.res));
+      outport.send(this.createIPBracket(IP.CLOSE));
+    }
+  }
+
+  wss.close();
+  this.dropIP(ip);
+}
+
+// TODO move globals into function:
+var rx = null;
+
+var wq = [];
+
+function handleWebRequest(req, res) {
+  console.log('got req url: ' + req.url);
+  wq.push({req: req, res: res});
+
+  if (!!rx) {
+    var q = wq;
+    wq = [];
+    rx(q);
+  }
+}
+
+function genListenFun(runtime, server, portno, proc) {
+  return function (done) {
+    // In next tick (TODO use process.nextTick() instead):
+    setTimeout(function() {
+      done();
+    }, 0);
+
+    server.listen(portno, function() { console.log('server listen cb'); });
+  };
+}
+
+function genWsReceiveFun(runtime, server, portno, proc) {
+  return function (done) {
+    rx = function(q) {
+      done(q);
+    }
+  };
+}
