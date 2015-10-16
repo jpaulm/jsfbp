e88a69413e73f593fbedbe22ce5432cdc85ed3a4
Paul Morrison <paul.morrison@rogers.com>
2015-10-15 21:34:28 -0400

    Connection capacities reduced to try to force deadlock

--- examples/testsubstreamsensitivesplitting.js
+++ examples/testsubstreamsensitivesplitting.js
@@ -19,19 +19,19 @@ var passthru2  = network.defProc('./components/passthru.js', 'passthru2');
 var makeMergeSubstreamSensitive = true;
 
 network.initialize(genss, 'COUNT', '400');
-network.connect(genss, 'OUT', lbal, 'IN', 50);
-network.connect(lbal, 'OUT[0]', passthru0, 'IN', 50);
-network.connect(lbal, 'OUT[1]', passthru1, 'IN', 50);
-network.connect(lbal, 'OUT[2]', passthru2, 'IN', 50);
+network.connect(genss, 'OUT', lbal, 'IN', 10);
+network.connect(lbal, 'OUT[0]', passthru0, 'IN', 1);
+network.connect(lbal, 'OUT[1]', passthru1, 'IN', 1);
+network.connect(lbal, 'OUT[2]', passthru2, 'IN', 1);
 
 /*
  * 3 passthru's feeding one port -> pretty mixed up data
  */  
 if (!makeMergeSubstreamSensitive) {
 	var recvr  = network.defProc('./components/recvr.js');
-      network.connect(passthru0, 'OUT', recvr, 'IN');
-      network.connect(passthru1, 'OUT', recvr, 'IN');
-      network.connect(passthru2, 'OUT', recvr, 'IN');
+      network.connect(passthru0, 'OUT', recvr, 'IN', 1);
+      network.connect(passthru1, 'OUT', recvr, 'IN', 1);
+      network.connect(passthru2, 'OUT', recvr, 'IN', 1);
 }
 else {
 	
@@ -41,9 +41,9 @@ else {
 	  var ssmerge  = network.defProc('./components/substreamsensitivemerge.js');
 	  var csws = network.defProc('./examples/components/checksequencewithinsubstreams.js');
 	  
-	  network.connect(passthru0, 'OUT', ssmerge, 'IN[0]');
-	  network.connect(passthru1, 'OUT', ssmerge, 'IN[1]');
-	  network.connect(passthru2, 'OUT', ssmerge, 'IN[2]');	
+	  network.connect(passthru0, 'OUT', ssmerge, 'IN[0]', 1);
+	  network.connect(passthru1, 'OUT', ssmerge, 'IN[1]', 1);
+	  network.connect(passthru2, 'OUT', ssmerge, 'IN[2]', 1);	
 	  network.connect(ssmerge, 'OUT', csws, 'IN');	  
 	  
 }
