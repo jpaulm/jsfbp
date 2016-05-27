7d7e34f6b79d13f4ef2a979f9416ee97455f8047
Dan Rumney <drumney@jellyvision.com>
2016-05-27 10:42:49 -0500

    Utils refactor

--- core/utils.js
+++ core/utils.js
@@ -1,13 +1,46 @@
 'use strict';
 
 var Fiber = require('fibers'),
-  ProcessStatus = require('./Process').Status;
+  ProcessStatus = require('./Process').Status,
+  trace = require('./trace');
+
+function getInportWithData(inportArray) {
+  var allDrained = true;
+
+  var inportElementWithData = inportArray.findIndex(function (inport) {
+    var conn = inport ? inport.conn : false;
+    if (!conn) {
+      return false;
+    }
+    else if (conn.usedslots > 0) {  // connection has data
+      return true;
+    }
+
+    allDrained = allDrained && conn.closed;  // no data but not all closed, so suspend
+    return false;
+  });
+
+  if (inportElementWithData >= 0) {
+    trace('findIPE_with_data - found: ' + inportElementWithData);
+  }
+  else if(allDrained) {
+    trace('findIPE_with_data: all drained');
+  } else {
+    inportElementWithData = null;
+  }
+
+  return {
+    inportElementWithData: inportElementWithData,
+    allDrained: allDrained
+  };
+}
 
 module.exports.getElementWithSmallestBacklog = function (array, elem) {
   var number = Number.MAX_VALUE;
   var element = elem;
-  if (element == -1)
+  if (element == -1) {
     element = 0;
+  }
   var j = element;
   for (var i = 0; i < array.length; i++) {
     if (array[j] == null || array[j] == undefined)
@@ -18,55 +51,29 @@ module.exports.getElementWithSmallestBacklog = function (array, elem) {
     }
     j = (j + 1) % array.length;
   }
-  //console.log('element: ' + element + '(' + number + ')');
   return element;
 };
 
 module.exports.findInputPortElementWithData = function (array) {
   var proc = Fiber.current.fbpProc;
 
-	if (tracing) {
-    console.log(proc.name + ' findIPE_with_data ');
-  }
+  trace('findIPE_with_data ');
 
   while (true) {
-		var element = -1;
-    var allDrained = true;
-		for (var i = 0; i < array.length; i++) {
-			if (array[i] == null || array[i] == undefined)
-				continue;
-			if (array[i].conn.usedslots > 0) {  // connection has data
-				element = i;
-				if (tracing) {
-				    console.log(proc.name + ' findIPE_with_data - found: ' + i);
-      }
-				return i;			
-      }
-			else if (array[i].conn.closed)  { 	// connection is drained				
-				continue;
-      }
-      else {
-        allDrained = false;                  // no data but not all closed, so suspend
-      }
-    }
-		if (allDrained) {
-			if (tracing) {
-        console.log(proc.name + ' findIPE_with_data: all drained');
-      }
-      return -1;
+    var inportWitData = getInportWithData(array);
+
+    if(inportWitData.inportElementWithData  !== null) {
+      return inportWitData.inportElementWithData;
     }
 
     proc.status = ProcessStatus.WAITING_TO_FIPE;
     proc.yielded = true;
-		if (tracing) {
-      console.log(proc.name + ' findIPE_with_data: susp');
-    }
+    trace('findIPE_with_data: susp');
+
     Fiber.yield();
     proc.status = ProcessStatus.ACTIVE;
     proc.yielded = false;
-		if (tracing) {
-      console.log(proc.name + ' findIPE_with_data: resume');
-    }
+    trace('findIPE_with_data: resume');
   }
 };
 
