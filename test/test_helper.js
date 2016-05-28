3624b1731391d209d3a89dd1a6dbc127a705f789
Dan Rumney <drumney@jellyvision.com>
2016-05-27 19:45:45 -0500

    InputPort unit Tests

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,9 +1,20 @@
 'use strict';
 
 var chai = require('chai');
+var Fiber = require('fibers');
+var Process = require('../core/Process');
 
 global.expect = chai.expect;
 
 global.MockSender = require('./mocks/MockSender');
 
 global.MockReceiver = require('./mocks/MockReceiver');
+
+global.TestFiber = function(action) {
+  Fiber(function() {
+    var mockProcess = new Process("test", function() {console.log("Test Process");});
+
+    Fiber.current.fbpProc = mockProcess;
+    action(mockProcess);
+  }).run();
+};
