6bad3bfd6439d2d72e8cc08e709dc43f0a3fdf4a
Dan Rumney <drumney@jellyvision.com>
2016-06-01 15:09:19 -0500

    New test that demonstrates that merge points are buggy

--- test/core/Network.js
+++ test/core/Network.js
@@ -75,5 +75,32 @@ describe('Network', function () {
       expect(result).to.deep.equal([1, 2]);
       done();
     });
-  })
+  });
+
+  it('supports merge points into the same InPort', function(done) {
+    var network = new Network();
+    var result = [];
+
+    var receiver = network.defProc(MockReceiver.generator(result), "RECVR");
+    var delay0 = network.defProc('jsfbp/delay', "DELAY0");
+    var delay1 = network.defProc('jsfbp/delay', "DELAY1");
+    var copy0 = network.defProc('jsfbp/copier', "COPY0");
+    var copy1 = network.defProc('jsfbp/copier', "COPY1");
+
+
+    network.connect(delay0, 'OUT', copy0, 'IN');
+    network.connect(delay1, 'OUT', copy1, 'IN');
+    network.connect(copy0, 'OUT', receiver, 'IN');
+    network.connect(copy1, 'OUT', receiver, 'IN');
+
+    network.initialize(delay0,'INTVL', 500);
+    network.initialize(delay0,'IN', 1);
+    network.initialize(delay1,'INTVL', 1000);
+    network.initialize(delay1,'IN', 2);
+
+    network.run(new fbp.FiberRuntime(), {trace: true}, function () {
+      expect(result).to.deep.equal([2, 1]);
+      done();
+    });
+  });
 });
