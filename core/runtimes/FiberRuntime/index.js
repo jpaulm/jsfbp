f66b9eea1f3a09695eec10f076548372605d6f93
Paul Morrison <jpaulmorr@gmail.com>
2016-06-01 19:10:07 -0400

    Testing alternative notation for network definitions

--- core/runtimes/FiberRuntime/index.js
+++ core/runtimes/FiberRuntime/index.js
@@ -134,38 +134,20 @@ FiberRuntime.prototype._genInitialQueue = function () {
   //console.log(self._list);  //xxx
   //console.log(Object.keys(self._list).length);
   // A process is selfstarting if its incoming ports are only connected to IIPs
- /* ---------------------------------
   self._list.forEach(function(process) {
-	console.log(process);  //xxx
-    var selfstarting = process.inports.reduce(function(currentlySelfstarting, inport) {
-      return currentlySelfstarting && (inport[1].conn instanceof IIPConnection);
-    }, true);
 
-    if(selfstarting) {
-        queue.push(process);
-      }
-      */
-   // -----------------------------
     var selfstarting = true;
-    for (var key in self._list) {
-    	  if (!(self._list.hasOwnProperty(key)))
-    	      continue;
-    	  //console.log(self._list[key]);
-    	  selfstarting = true;
-    	  self._list[key].inports.forEach(function(port) {
-    		    if (!(port[1].conn instanceof IIPConnection ))
-    		       selfstarting = false;
-    		  } , this);
-
-          if(selfstarting) {
-             queue.push(self._list[key]);
-          }
+    process.inports.forEach(function(inPort) {
+      selfstarting = selfstarting && inPort.conn instanceof IIPConnection;
+    });
+    
+    if(selfstarting) {
+      queue.push(process);
     }
-//--------------------------
-
+  });
 
   return queue;
-}
+};
 
 
 FiberRuntime.prototype._procState = function (proc) {
@@ -193,7 +175,7 @@ FiberRuntime.prototype._actualRun = function () {
   var setPortRuntime = function (port) {
     port[1].setRuntime(this);
   }.bind(this);
-
+  
   this._list.forEach(function (process) {
     process.inports.forEach(setPortRuntime);
     process.outports.forEach(setPortRuntime);
